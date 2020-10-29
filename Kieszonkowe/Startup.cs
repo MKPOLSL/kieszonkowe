9a9ae1d7967ee739b4b0bc596f9ee83fa7388ece
Mateusz Kulinowski <mat.kulinowski@gmail.com>
2020-10-29 20:32:17 +0100

    delete line

--- Kieszonkowe/Startup.cs
+++ Kieszonkowe/Startup.cs
@@ -32,7 +32,6 @@ namespace Kieszonkowe
 
             services.AddSingleton<IStatisticsService, StatisticsService>();
             services.AddScoped<IChildRecordService, ChildRecordService>();
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
